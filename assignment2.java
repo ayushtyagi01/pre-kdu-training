c2dcfcd5d8656de5ed9b33b2b2e574b7220a9aa8
ayushtyagi01 <54800927+ayushtyagi01@users.noreply.github.com>
2022-12-15 23:25:19 +0530

    Exercise 2-Java

--- assignment2.java
+++ assignment2.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class Java2 {
+class Java2{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<String> arr = new ArrayList<>();
